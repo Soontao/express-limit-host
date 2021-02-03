f9433018f9d1ccd27dc5a88d3cf570a84f7c5857
Theo Sun <theo.sun@sap.com>
2021-02-03 11:40:15 +0800

    feat: v1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
+import { Handler } from "express";
+import createError from "http-errors";
 
-export const a = 1;
+export interface Options {
+  loopback?: boolean;
+  whitelists?: Array<string>;
+}
 
+const defaultOption: Options = {
+  loopback: true,
+  whitelists: []
+};
+
+const LOOPBACK_HOSTS = ['localhost', '127.0.0.1', '::1'];
+
+export const limitForwardedHost = (option: Options = defaultOption): Handler => {
+  option = Object.assign(defaultOption, option);
+
+  return (req, res, next) => {
+    const { hostname } = req;
+    if (option.loopback) {
+      if (LOOPBACK_HOSTS.includes(hostname)) {
+        return next();
+      }
+    }
+    if (option.whitelists.includes(hostname)) {
+      return next();
+    } else {
+      return next(createError(403, `${hostname} is not trusted for proxy`));
+    }
+  };
+};
 
